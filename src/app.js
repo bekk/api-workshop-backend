7a3bed147b883166478260af216312f58dea9c47
Herman M. Lund <hmlund@users.noreply.github.com>
2021-08-22 12:08:28 +0200

    Skriver om fra ts til js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { getTilsynList } from "./tilsynList";
+
+const app = express();
+
+const port = 3003;
+
+app.get("/tilsyn", (req, res) => {
+  res.send(getTilsynList());
+});
+
+app.get("/tilsyn/:tilsynId", (req, res) => {
+  console.log(req.params.tilsynId);
+  res.send(req.params);
+});
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port} !`);
+});
